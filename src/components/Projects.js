fcf08bdd1007d5dad03d3d54a5d3a59e86762aaf
Akhil Bachubhay <akhil.bachubhay@avanade.com>
2023-01-05 01:05:16 -0500

    update skills and projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -23,12 +23,7 @@ export default function Projects() {
               key={project.image}
               className="sm:w-1/2 w-100 p-4">
               <div className="flex relative">
-                <img
-                  alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
-                  src={project.image}
-                />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-300 bg-gray-800 opacity-0 hover:opacity-100">
+                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-300 bg-gray-800 opacity-95 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-my-blue mb-1">
                     {project.subtitle}
                   </h2>
---
+++ src/data.js
@@ -0,0 +1,31 @@
+export const projects = [
+  {
+    title: "GymBro",
+    subtitle: "React Native",
+    description:
+      "A front-end mobile application with posts, profile management, and messaging, based on the idea of a fitness based social media platform.",
+    image: "./gymbro-pic.png",
+    link: "https://github.com/abachubhay255/GymBro",
+  },
+  {
+    title: "Movie Paradise",
+    subtitle: "MEAN Stack",
+    description:
+      "An organizational movie web app with user management, CRUD functionality for movies and lists, and search using TheMovieDB API.",
+    image: "./movie_paradise_demo.gif",
+    link: "https://github.com/abachubhay255/MovieParadiseProject",
+  },
+];
+
+export const skills = [
+  "React | React Native",
+  "TypeScript | JavaScript",
+  "GraphQL",
+  "Microsoft Azure",
+  "Node.js",
+  "HTML | CSS",
+  "Python",
+  "Angular",
+  "Java",
+  "MongoDB",
+];
